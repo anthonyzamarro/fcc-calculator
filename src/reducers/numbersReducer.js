7933d9dc30dd1bd14208b25971ee776554e4c25b
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2018-11-21 08:16:06 -0500

    update numbersReducer

--- src/reducers/numbersReducer.js
+++ src/reducers/numbersReducer.js
@@ -1,22 +1,25 @@
-// function evaluateArray(array) {
-// 	let calculate = array.map(item => {
-// 		console.log(item);
-// 	});
-// 	return calculate;
-// }
+function evaluateArray(array) {
+	for (var i = array.length - 1; i >= 0; i--) {
+		let newArr = [];
+		let convertNum = parseFloat(array[i]);
+		let convertNextNum = parseFloat(array[i + 1]);
+		if (Number.isInteger(convertNextNum)) {
+			console.log('convertNume',convertNum, 'convertNextNum',convertNextNum);
+		}
+	}
+	console.log(array)
+	return 'calculate';
+}
 
 export default function(state = [], action) {
   switch (action.type) {
     case "NUMBER_CLICKED":
 	    let numberArray = state.slice();
 	    numberArray.push(action.payload);
-
-		let filtered = numberArray.filter((n,i) => {
-			
-	    });
-
-
-	    // console.log('NUMBER_CLICKED',numberArray);
+	    let zeroFirst = numberArray[0];
+	    if (zeroFirst === '0') {
+	    	numberArray.shift();
+	    }
 	    return numberArray;
 	case 'OPERATOR_CLICKED':
 		let operatorArray = state.slice();
@@ -25,14 +28,14 @@ export default function(state = [], action) {
 	  	return operatorArray;
 	case 'EQUALS_CLICKED':
 		let equalsArray = state.slice();
-		// evaluateArray(equalsArray);
+		evaluateArray(equalsArray);
 		// console.log(blah)
 		// let val = equalsArray.join('');
-		return [null];
+		return [2];
 	case 'CLEAR_CLICKED':
 		let clearArray = state.slice();
 		// console.log('CLEAR_CLICKED', clearArray);
-		clearArray = [0];
+		clearArray = [];
 		return clearArray;
     default:
       return state;
