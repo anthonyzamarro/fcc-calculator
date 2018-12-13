fd98f76ba43cf32160dbaf18b7d0d600fb161668
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2018-12-12 21:50:59 -0500

    Updates

---
+++ precache-manifest.294f63a341960bc40772c0e7000da3df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4ae93efedb64cc1a421",
+    "url": "/static/js/main.f4ae93ef.chunk.js"
+  },
+  {
+    "revision": "a243410ccf4f91201170",
+    "url": "/static/js/1.a243410c.chunk.js"
+  },
+  {
+    "revision": "f4ae93efedb64cc1a421",
+    "url": "/static/css/main.4b565213.chunk.css"
+  },
+  {
+    "revision": "c0a413af52a74eeee8e3ffdba97f1fa0",
+    "url": "/index.html"
+  }
+];
